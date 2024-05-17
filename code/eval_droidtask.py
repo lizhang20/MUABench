17ab06d4aa40c453a629fcafe7fd7bc380285e92
Shihe Wang <wshprimy@gmail.com>
2024-05-17 13:10:01 +0800

    feat: mmlu style

--- code/eval_droidtask.py
+++ code/eval_droidtask.py
@@ -11,7 +11,7 @@ class Task(NamedTuple):
     task_description: str
     ui_representation: str
     action_index: int
-    action_param: str
+    total_index: int
 
 
 def load_tasks_from_file(filepath: str):
@@ -30,12 +30,26 @@ def load_tasks_from_file(filepath: str):
         task_description = data["task_name"]
 
         for record in data["records"]:
+            if record["Input"] != "null":
+                continue
+            assert (
+                record["Input"] == "null"
+            ), f"Record text input is not null: {record['Input']}"
+
+            line_last = record["State"].splitlines()[-1]
+            total_index = re.search(r"id=(\d+)", line_last).group(1)
+            total_index = int(total_index) + 1
+
+            action_index = int(record["Choice"])
+            action_index = action_index if action_index != -1 else total_index
+            # print(f"UI = {record['State']}\nTotal = {total_index}")
+
             tasks.append(
                 Task(
                     task_description=task_description,
                     ui_representation=record["State"],
-                    action_index=record["Choice"],
-                    action_param=record["Input"],
+                    action_index=action_index,
+                    total_index=total_index,
                 )
             )
 
@@ -79,135 +93,56 @@ def load_all_tasks(task_path: str):
 
 
 def construct_query_prompt_zero_shot(task: Task):
-    prompt = f"""Given a screen, an instruction, predict the id of the UI element to perform or continue the instruction.
-If you think it needs to input text, also give the text, else return the string 'null'.
-Your output should only contain the folliwing information:
-[id], '[input]'
-
-Screen:
-{task.ui_representation}
-
-Instruction: {task.task_description}
-"""
-    return prompt
-
-
-def construct_query_prompt_one_shot(task: Task):
-    prompt = f"""Given a screen, an instruction, predict the id of the UI element to perform or continue the instruction.
-If you think it needs to input text, also give the text, else give the string 'null'.
-Your output should only contain the folliwing information:
-[id], '[input]'
-
-Screen:
-{task.ui_representation}
-
-Instruction: {task.task_description}
-
-The following are examples:
-
-Screen:
-<input id=0 >Search</input>\n <button id=1 text='Sort by'></button>\n <button\
-    \ id=2 text='Toggle app name visibility'></button>\n <button id=3 text='More options'></button>\n\
-    \ <button id=4>Calendar</button>\n <button id=5>Camera</button>\n <button id=6>Clock</button>\n\
-    \ <button id=7>go back</button>
-
-Instruction: Sort apps by title in descending order
-
-Output:
-1, 'null'
-"""
-    return prompt
-
-
-def construct_query_prompt_two_shots(task: Task):
-    prompt = f"""Given a screen, an instruction, predict the id of the UI element to perform or continue the instruction.
-If you think it needs to input text, also give the text, else give the string 'null'.
-Your output should only contain the folliwing information:
-[id], '[input]'
-
-Screen:
-{task.ui_representation}
-
-Instruction: {task.task_description}
-
-The following are examples:
-
-Example 1:
-
-Screen:
-<input id=0 >Search</input>\n <button id=1 text='Sort by'></button>\n <button\
-    \ id=2 text='Toggle app name visibility'></button>\n <button id=3 text='More options'></button>\n\
-    \ <button id=4>Calendar</button>\n <button id=5>Camera</button>\n <button id=6>Clock</button>\n\
-    \ <button id=7>go back</button>
-
-Instruction: Sort apps by title in descending order
-
-Output:
-1, 'null'
-
-Example 2:
-
-Screen:
-<button id=0 text=''Search''></button>
-
-    <button id=1 text=''Open note''></button>
-
-    <button id=2 text=''Create a new note''></button>
-
-    <button id=3 text=''More options''></button>
-
-    <button id=4>General note</button>
-
-    <p id=5>test</p>
-
-    <input id=6>Insert text here</input>
-
-    <button id=7>go back</button>
-
-Instruction: Create a text note called 'test', type '12345678', and search for '234'
-
-Output:
-6, '123456'
+    prompt = f"""\
+Screen description: {task.ui_representation}
+If I want to complete that {task.task_description}, I need to operate
 """
     return prompt
 
 
 def extract_and_compare_query_results(response: str, task: Task) -> bool:
     try:
-        id, input = int(response.split(", ")[0]), response.split(", ")[1].strip(
-            "'"
-        ).strip('"')
+        id = int(response)
     except ValueError:
         return False
+    return id == task.action_index
 
-    # print(f"extracting {id} {input} from response: {response}")
-    if id == task.action_index and input == task.action_param:
-        return True
 
-    return False
+def query_llm(prompt: str) -> str:
+    """Query the LLM which has been tuned by instructions."""
+
+    SYSPROMPT = "Only output a number.\n"
+    prompt = SYSPROMPT + prompt
+    # return "1"
+
+    client = OpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY"),
+        base_url=os.environ.get("OPENAI_BASE_URL"),
+    )
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        seed=0,
+        temperature=0,
+        messages=[
+            {"role": "user", "content": prompt},
+        ],
+        timeout=30,
+    )
+    response = completion.choices[0].message.content
+    return response
 
 
 def test_with_local_model():
-    tasks: List[Task] = load_all_tasks("../DroidTask")
+    """mmlu
+    doc_to_text: construct_query_prompt_zero_shot(task)
+    doc_to_choice: [for i in range(0, task.total_index)]
+    doc_to_target: task.action_index
+    """
+    tasks: List[Task] = load_all_tasks("DroidTask")
 
-    for index, task in enumerate(tasks[:10]):
-        prompt = construct_query_prompt_one_shot(task)
-        client = OpenAI(
-            api_key=os.environ.get("OPENAI_API_KEY"),
-            base_url=os.environ.get("OPENAI_BASE_URL"),
-        )
-
-        completion = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            seed=0,
-            temperature=0,
-            messages=[
-                {"role": "user", "content": prompt},
-            ],
-            timeout=30,
-        )
-        response = completion.choices[0].message.content
-
+    for index, task in enumerate(tasks):
+        prompt = construct_query_prompt_zero_shot(task)
+        response = query_llm(prompt)
         print(
             f"task-{index}, success: {extract_and_compare_query_results(response, task)}"
         )
